52ef04c5196e5125aa666732000688f0cec4d6a3
MerlinsArul <merlins.arul@aspiresys.com>
2023-04-03 17:14:51 +0530

    NavBar update

---
+++ src/Components/Header/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+      title: 'Mobiles',
+      path: '/mobiles',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Laptop',
+      path: '/laptop',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Television',
+      path: '/televison',
+      cName: 'dropdown-link'
+    },
+  ];
--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,12 +1,98 @@
-import React from 'react';
-import Logo from '../Logo';
+import React, { useState } from "react";
+// import { Button } from './Button';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Logo from "../Logo";
+import Dropdown from "./Dropdown";
 
 function Navbar(props) {
-    return (
-        <header>
-       <Logo/> 
-        </header>
-    );
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  return (
+    <>
+      <header>
+        <Logo />
+        <nav className="navbar">
+          {/* <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div> */}
+        
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            
+            <li className="nav_searchbar">
+              <input className="mysearch"
+                type="text"
+                name=""
+                //  onChange={(e) => getText(e.target.value)}
+                placeholder="Search Your Products"
+                 /><button className="searchbtn">Search</button>
+            </li>
+      
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li
+              className="nav-item"
+              onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave}
+            >
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Category
+                <i className="fas fa-caret-down" />
+              </Link>
+              {dropdown && <Dropdown />}
+            </li>
+
+            <li className="nav-item">
+              <Link
+                to="/Login"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+               Login
+              </Link>
+            </li>
+            {/* <li>
+            <Link
+              to='/sign-up'
+              className='nav-links-mobile'
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li> */}
+          </ul>
+          {/* <Button /> */}
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Navbar;
